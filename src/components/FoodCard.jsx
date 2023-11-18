5b9177c6ea813cb67bb27a6e20ba53b91d4e5ad5
oduwole ayomipo <harkorede318@gmail.com>
2023-11-18 01:06:55 +0100

    card ui

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import '../styles/FoodCard.css'
+import '../App.css'
+import GreekSalad from '../images/greek-salad.svg'
 
 function FoodCard() {
   return (
-    <div>
-        somecard
+    <div className='card'>
+        <img src={GreekSalad} alt="" className='card-image' />
+        <div className='card-description'>
+            <div>
+                <div className='card-top-description'>
+                    <h4>Greek Salad</h4>
+                    <p>$13.99</p>
+                </div>
+                <p>The famous Greek salad of crispy lettuce, green pepper, olives and our Chicago finest bell pepper and tomato.</p>
+            </div>
+            <div className='card-footer'>
+                <p> Order a Delivery</p>
+            </div>
+        </div>
     </div>
   )
 }
